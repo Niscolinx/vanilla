e162595c2c409096ef834a8125c344337a40e0e8
Igboanugwo Collins <munisco12@gmail.com>
2021-06-22 09:48:22 +0100

    working on the form

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.form__btn')
+const firstName = document.querySelector('.form__input')
+const btn = document.querySelector('.form__btn')
+const form = document.querySelector('#form')
+
+
+
+
